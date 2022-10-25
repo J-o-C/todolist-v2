a9057ed2a53da956f44e0c2389b3d2c86100aa60
Jose Socorro <jcarlossoorro@gmail.com>
2022-10-24 21:42:08 -0400

    Commit to prepare for heroku

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolist", {useNewUrlParser: true});
+const uri = "mongodb+srv://jose:1234@cluster0.conr5ph.mongodb.net/todolist";
+
+mongoose.connect(uri, {useNewUrlParser: true});
 
 const itemSchema = new mongoose.Schema({
   name: String
@@ -99,6 +101,11 @@ app.get("/lists/:list", async (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
